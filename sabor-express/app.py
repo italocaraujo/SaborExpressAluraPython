bd785104fc029bf95f877ac5ed68f7e9241f0393
Italo Correia <128881501+italocaraujo@users.noreply.github.com>
2024-10-08 15:58:27 -0300

    Add files via upload

--- sabor-express/app.py
+++ sabor-express/app.py
@@ -1,5 +1,7 @@
 import os
 
+restaurantes = []
+
 def exibir_nome_do_programa():
     print("""
         ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
@@ -14,27 +16,46 @@ def exibir_opcoes():
     print('1. Cadastrar restaurante')
     print('2. Listar restaurante')
     print('3. Ativar restaurante')
-    print('1. Sair')
+    print('4. Sair')
 
 def finalizar_app():
     os.system('cls')
     # os.system('clear')
     print('Encerrando o programa\n')
 
-def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: '))
-    # opcao_escolhida = int(opcao_escolhida)
+def opcao_invalida():
+    print('Opção inválida!\n')
+    input('Digite uma tecla para voltar ao menu principal')
+    main()
 
-    if opcao_escolhida == 1:
-        print('Cadastrar restaurante')
-    elif opcao_escolhida == 2:
-        print('Listar restaurantes')
-    elif opcao_escolhida == 3:
-        print('Ativar restaurantes')
-    else: 
-        finalizar_app()
+def cadastrar_novo_restaurante():
+    os.system('cls')
+    print('Cadastro de novos restaurantes\n')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+    restaurantes.append(nome_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
+    input('\nDigite uma tecla para voltar ao menu principal')
+    main()
+
+def escolher_opcao():
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        # opcao_escolhida = int(opcao_escolhida)
+
+        if opcao_escolhida == 1:
+            cadastrar_novo_restaurante();
+        elif opcao_escolhida == 2:
+            print('Listar restaurantes')
+        elif opcao_escolhida == 3:
+            print('Ativar restaurantes')
+        elif opcao_escolhida == 4: 
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except: opcao_invalida()
 
 def main():
+    os.system('cls')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
