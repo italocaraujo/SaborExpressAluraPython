22c9737399682bcfec12930e4584091d88603571
Italo Correia <128881501+italocaraujo@users.noreply.github.com>
2024-10-08 23:34:13 -0300

    Add files via upload

--- sabor-express/app.py
+++ sabor-express/app.py
@@ -1,6 +1,9 @@
 import os
 
-restaurantes = []
+restaurantes = [{'nome':'Praça', 'categoria':'Japonesa', 'ativo':False},
+                {'nome':'Pizza Suprema', 'categoria':'Pizzaria', 'ativo':True},
+                {'nome':'Cantina', 'categoria':'Italiana', 'ativo':False}
+]
 
 def exibir_nome_do_programa():
     print("""
@@ -15,27 +18,79 @@ def exibir_nome_do_programa():
 def exibir_opcoes():
     print('1. Cadastrar restaurante')
     print('2. Listar restaurante')
-    print('3. Ativar restaurante')
+    print('3. Alternar estado do restaurante')
     print('4. Sair')
 
 def finalizar_app():
-    os.system('cls')
+    exibir_subtitulo('Encerrando o programa...')
     # os.system('clear')
-    print('Encerrando o programa\n')
+
+def voltar_ao_menu_principal():
+    input('\nDigite uma tecla para voltar ao menu principal ')
+    main()
 
 def opcao_invalida():
     print('Opção inválida!\n')
-    input('Digite uma tecla para voltar ao menu principal')
-    main()
+    voltar_ao_menu_principal()
+
+def exibir_subtitulo(texto):
+    os.system('cls')
+    linha = '*' * (len(texto))
+    print(linha)
+    print(texto)
+    print(linha)
+    print() # Pula uma linha
 
 def cadastrar_novo_restaurante():
-    os.system('cls')
-    print('Cadastro de novos restaurantes\n')
+    '''Essa função é responsável por cadastrar um novo restaurante
+    - Nome do restaurante
+    - Categoria
+
+    Output:
+    - Adiciona um novo restaurante à lista de restaurantes
+
+    '''
+    exibir_subtitulo('Cadastro de novos restaurantes')
+
     nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
-    restaurantes.append(nome_do_restaurante)
+    categoria = input(f'Digite a categoria do restaurante {nome_do_restaurante}: ')
+    dados_do_restaurante = {'nome':nome_do_restaurante, 'categoria':categoria, 'ativo':False}
+
+    restaurantes.append(dados_do_restaurante)
     print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
-    input('\nDigite uma tecla para voltar ao menu principal')
-    main()
+    
+    voltar_ao_menu_principal()
+
+def listar_restaurantes():
+    exibir_subtitulo('Listando os restaurantes')
+    ''' Essa função é responsável por listar todos os restaurantes cadastrados '''
+
+    print(f"{'Nome do restaurante'.ljust(22)} | {'Categoria'.ljust(202)} | Status")
+    for restaurante in restaurantes:
+        nome_restaurante = restaurante['nome']
+        categoria = restaurante['categoria']
+        ativo = 'ativado' if restaurante['ativo'] else 'desativado'
+        print(f'- {nome_restaurante.ljust(20)} | {categoria.ljust(20)} | {ativo}')
+
+    voltar_ao_menu_principal()
+
+def alternar_estado_restaurante():
+    exibir_subtitulo('Alterando estado do restaurante')
+    ''' Essa função é responsável por alterar o estado de ativo ou inativo dos restaurantes '''
+
+    nome_restaurante = input('Digite o nome do restaurante que deseja alternar o estado: ')
+    restaurante_encontrado = False
+
+    for restaurante in restaurantes:
+        if nome_restaurante == restaurante['nome']:
+            restaurante_encontrado = True
+            restaurante['ativo'] = not restaurante['ativo']
+            mensagem = f'O restaurante {nome_restaurante} foi ativado com sucesso' if restaurante['ativo'] else f'O restaurante {nome_restaurante} foi desativado com sucesso'
+            print(mensagem)
+    if not restaurante_encontrado:
+        print('O restaurante não foi encontrado')
+
+    voltar_ao_menu_principal()
 
 def escolher_opcao():
     try:
@@ -45,9 +100,9 @@ def escolher_opcao():
         if opcao_escolhida == 1:
             cadastrar_novo_restaurante();
         elif opcao_escolhida == 2:
-            print('Listar restaurantes')
+            listar_restaurantes()
         elif opcao_escolhida == 3:
-            print('Ativar restaurantes')
+            alternar_estado_restaurante()
         elif opcao_escolhida == 4: 
             finalizar_app()
         else:
