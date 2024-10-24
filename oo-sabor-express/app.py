8c768486bfcf9a31d68c309acedf319b20446023
Italo Correia <128881501+italocaraujo@users.noreply.github.com>
2024-10-24 15:41:24 -0300

    Add files via upload

--- oo-sabor-express/app.py
+++ oo-sabor-express/app.py
@@ -1,6 +1,7 @@
 from modelos.restaurante import Restaurante
 from modelos.cardapio.bebida import Bebida
 from modelos.cardapio.prato import Prato
+from modelos.cardapio.sobremesa import Sobremesa
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
 # restaurante_mexicano = Restaurante('Mexican Food', 'Mexicana')
@@ -13,13 +14,16 @@ restaurante_praca = Restaurante('praça', 'Gourmet')
 # restaurante_praca.receber_avaliacao('Stenio', 2)
 
 bebida_suco = Bebida('Suco de Melancia', 5.0, 'grande')
-prato_paozinho = Prato('Pãozinho', 2.00, 'O melhor pão da cidade')
+prato_paozinho = Prato('Pãozinho', 2.0, 'O melhor pão da cidade')
+
+bebida_suco.aplicar_desconto()
+prato_paozinho.aplicar_desconto()
 
 restaurante_praca.adicionar_no_cardapio(bebida_suco)
-restaurante_praca.adicionar_prato_no_cardapio(prato_paozinho)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
 def main():
-    pass
+    restaurante_praca.exibir_cardapio
 
 if __name__ == '__main__':
     main()
